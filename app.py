7e413d610eda4e9b0465545720d17f039ef72abf
Kamal Ghimire <kamalghimire121@gmail.com>
2021-04-11 17:13:34 -0400

    added index file using render_template

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''
-    <h1>Kamal Ghimire</h1>
-    '''
+    return render_template('index.html')
 
 
 
