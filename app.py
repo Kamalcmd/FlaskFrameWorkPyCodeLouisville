59397fd5ed6a910f1b302b62d79aa7955017fde4
Kamal Ghimire <kamalghimire121@gmail.com>
2021-04-15 09:28:07 -0400

    install flask sqlAlchemy and create requirement.txt

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/404_Error')
+def error_page():
+    return render_template('404.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+
+@app.route('/portfolio')
+def portfolio_page():
+    return render_template('portfolio.html')    
 
 
 
